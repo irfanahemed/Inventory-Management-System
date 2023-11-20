429657e54558e11157288d998769f85d82ba42ac
harishy0406 <142865295+harishy0406@users.noreply.github.com>
2023-11-20 22:16:00 +0530

    Add files via upload

---
+++ src/ism/ISM.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ism;
+
+/**
+ *
+ * @author azmil
+ */
+public class ISM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Loginpage LoginpageFrame = new Loginpage();
+        LoginpageFrame.setVisible(true);
+        LoginpageFrame.pack();
+        LoginpageFrame.setLocationRelativeTo(null);
+    }
+    
+}
+
+   
+
